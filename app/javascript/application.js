56ec65348a64c5632337cba8888eec6e44777d4a
Ghali Dhobb <ghalidhobb@gmail.com>
2023-12-07 23:36:58 +0100

    tryout of itemslide

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -897,6 +897,57 @@ document.addEventListener('DOMContentLoaded', function() {
   let svgs = document.querySelectorAll('[id^="svg"]');
   let items = document.querySelectorAll('.item');
 
+
+
+  // Fonction pour vérifier si l'écran est un mobile
+  function estMobile() {
+  return window.innerWidth <= 768; // Vous pouvez ajuster cette valeur en fonction de vos besoins
+  }
+
+  // Fonction à appliquer uniquement en mode mobile
+  function fonctionMobile() {
+    // ITEM MOBILE SLIDE FUNCTION
+
+    let left = document.querySelector('.arrow-left');
+    let right = document.querySelector('.arrow-right');
+    let indexItem = 0;
+    displayItem(indexItem); // Move this line after the displayItem function definition
+
+    function displayItem(indexItem) {
+      items.forEach((item) => {
+        item.classList.display = 'none';
+      });
+      items[indexItem].style.display = 'flex';
+    }
+
+    function nextItem() {
+      indexItem++;
+      if (indexItem > items.length - 1) {
+        indexItem = 0;
+      }
+      displayItem(indexItem);
+    }
+
+    function prevItem() {
+      indexItem--;
+      if (indexItem < 0) {
+        indexItem = items.length - 1;
+      }
+      displayItem(indexItem);
+    }
+
+    right.addEventListener('click', nextItem);
+    left.addEventListener('click', prevItem);
+
+      console.log("Ceci s'exécute uniquement sur mobile");
+  }
+
+  // Vérifier si l'écran est un mobile avant d'exécuter la fonction
+  if (estMobile()) {
+  fonctionMobile();
+  }
+
+
   // Ajoutez des gestionnaires d'événements pour chaque rubrique
   rubriques.forEach(function(rubrique, index) {
     rubrique.addEventListener('click', function() {
@@ -1060,6 +1111,6 @@ function prevSlide() {
 next.addEventListener('click', nextSlide); 
 prev.addEventListener('click', prevSlide); 
 
-// CODE POUR PORTFOLIO 
+
 
 
