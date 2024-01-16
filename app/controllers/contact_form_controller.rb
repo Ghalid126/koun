e94e1ee0efbfc0fe6df26cf31b3c9eaca22edfcb
Ghali Dhobb <ghalidhobb@gmail.com>
2024-01-16 10:34:09 +0100

    add meta-tag to contact

--- app/controllers/contact_form_controller.rb
+++ app/controllers/contact_form_controller.rb
@@ -1,7 +1,23 @@
 class ContactFormController < ApplicationController
   def new
     # This action will render your form
+      set_meta_tags title: I18n.t('meta.contact.title'),
+                description: I18n.t('meta.contact.description'),
+                keywords: I18n.t('meta.contact.keywords'),
+                og: {
+                  title: I18n.t('meta.contact.og.title'),
+                  description: I18n.t('meta.contact.og.description'),
+                  url: I18n.t('meta.contact.og.url'),
+                  type: I18n.t('meta.contact.og.type')
+                },
+                twitter: {
+                  card: I18n.t('meta.contact.twitter.card'),
+                  title: I18n.t('meta.contact.twitter.title'),
+                  description: I18n.t('meta.contact.twitter.description')
+                }
   end
+
+
 
   def create
     # Extract form data
